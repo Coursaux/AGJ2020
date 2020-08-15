64248b72d3d6223a11abf2555855427da30ee8ea
COLEPARKERPC\Cole <cole.parker@hotmail.ca>
2020-08-14 20:25:54 -0600

    removed print statement

--- AGJ2020/Assets/Scripts/PlayerController.cs
+++ AGJ2020/Assets/Scripts/PlayerController.cs
@@ -39,7 +39,6 @@ public class PlayerController : MonoBehaviour
         {
             transform.position -= transform.right * negativeSpeed * Time.deltaTime;
         }
-        print(speed);
     }
 
     private void HandleTilt()
