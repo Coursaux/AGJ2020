6700d88868c554874957d5202e3bea82b91d7319
COLEPARKERPC\Cole <cole.parker@hotmail.ca>
2020-08-15 13:45:25 -0600

    camera shake issue fixed

---
+++ AGJ2020/Assets/Scripts/CameraShaker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShaker : MonoBehaviour
+{
+    bool cameraShaking;
+
+    public void ShakeCamera(float shakeDuration, float shakeIntensity)
+    {
+        StartCoroutine(ShakeCoroutine(shakeDuration, shakeIntensity));
+    }
+
+    IEnumerator ShakeCoroutine(float shakeDuration, float shakeIntensity)
+    {
+        cameraShaking = true;
+        Vector3 originalPosition = transform.position;
+        while (shakeDuration > 0)
+        {
+            transform.position = originalPosition + Random.insideUnitSphere * shakeIntensity;
+            shakeDuration -= Time.deltaTime;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        cameraShaking = false;
+    }
+}
