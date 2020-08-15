47802190532f78d568a3d89a5cec1f8dfbf05dda
COLEPARKERPC\Cole <cole.parker@hotmail.ca>
2020-08-15 13:16:47 -0600

    made meteors stop spawning on win

--- AGJ2020/Assets/Scripts/Egg.cs
+++ AGJ2020/Assets/Scripts/Egg.cs
@@ -20,6 +20,8 @@ public class Egg : MonoBehaviour
         player.SetSpeed(0);
         player.playingGame = false;
         player.head.playingGame = false;
+        player.GetComponent<MeteorSpawner>().minTime = Mathf.Infinity;
+        player.GetComponent<MeteorSpawner>().maxTime = Mathf.Infinity;
         player.GetComponent<HealthManager>().WinGame();
     }
 }
