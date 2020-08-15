63e437b4020a6d1a06c7deb9005f111643fe0ea6
COLEPARKERPC\Cole <cole.parker@hotmail.ca>
2020-08-15 11:24:37 -0600

    Instructions Placeholder Implemented

--- AGJ2020/Assets/Scripts/MainMenu.cs
+++ AGJ2020/Assets/Scripts/MainMenu.cs
@@ -1,15 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called before the first frame update\
+
+    [SerializeField] Button backButton;
+    [SerializeField] Button playGameButton;
+    [SerializeField] Button instructionsButton;
+    [SerializeField] TextMeshProUGUI instructionsText;
+
+    private void Start()
+    {
+        backButton.gameObject.SetActive(false);
+        instructionsText.gameObject.SetActive(false);
+    }
 
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void InstructionsClick()
+    {
+        backButton.gameObject.SetActive(true);
+        instructionsText.gameObject.SetActive(true);
+        playGameButton.gameObject.SetActive(false);
+        instructionsButton.gameObject.SetActive(false);
+    }
+
+    public void BackClick()
+    {
+        backButton.gameObject.SetActive(false);
+        instructionsText.gameObject.SetActive(false);
+        playGameButton.gameObject.SetActive(true);
+        instructionsButton.gameObject.SetActive(true);
     }
 
 }
